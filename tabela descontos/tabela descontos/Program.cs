af2e4c38bf71671a0e7b0605e925c5bbae790941
cleifer das chagas <151791542+chagas1580@users.noreply.github.com>
2023-11-22 22:01:12 -0300

    console em C# para tabela de descontos

---
+++ tabela descontos/tabela descontos/Program.cs
@@ -0,0 +1,17 @@
+﻿float preco;
+int desconto;
+string pag;
+string produto;
+Console.WriteLine("escolha seu produto:\n vassoura \n cerveja \n suco \n refrigerante \n carro ");
+produto = Console.ReadLine(); 
+if (produto == "cerveja")
+{
+    Console.WriteLine("o preco da gelada é 5 pila");
+
+}
+Console.WriteLine("escolha a forma de pagamento:");
+pag = Console.ReadLine();
+if (pag== "pix")
+{
+    Console.WriteLine("te faco por 2 pila");
+}
