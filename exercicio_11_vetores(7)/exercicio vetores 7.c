170fec1614013d4fd767e054834058694425ea26
cleifer chagas <cleifer.chagas@hotmail.com>
2023-12-07 14:15:44 -0300

    vetores

---
+++ exercicio_11_vetores(7)/exercicio vetores 7.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main() {
+printf("-----vetores7.0-----\n");
+int a [10];
+a[0] = 100;
+a[1] = 90;
+a[2] = 80;
+a[3] = 70;
+a[4] = 60;
+a[5] = 50;
+a[6] = 40;
+a[7] = 30;
+a[8] = 20;
+a[9] = 10;
+printf("os valores atribuidos ao vetor sao: \n %d\n %d\n %d\n %d\n %d\n %d\n %d\n %d\n %d\n %d\n",a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9] );
+
+return 0;
+
+}
